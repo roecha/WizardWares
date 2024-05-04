175f7642f937483ff10f6d3cf03cdc01bb5da264
roecha <croehl23@gmail.com>
2024-05-04 14:12:37 -0500

    Added product model

--- WizardWares.Models/Product.cs
+++ WizardWares.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace WizardWares.Models
 {
     internal class Product
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double PriceMoney { get; set; }
+        public string PriceObject { get; set; }
+        //public int CategoryId { get; set; }
+        //[ForeignKey("CategoryId")]
+        //[ValidateNever]
+        //public Category Category { get; set; }
+        //[ValidateNever]
+        public string ImageUrl { get; set; }
     }
 }
