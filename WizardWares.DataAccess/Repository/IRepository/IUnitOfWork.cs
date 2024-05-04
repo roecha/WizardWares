5b107ab4d05a37fcde5445110d9a6a9d6139b9a6
roecha <croehl23@gmail.com>
2024-05-04 14:37:46 -0500

    Added a little bit to front end. Category page doesn't show

--- WizardWares.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WizardWares.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WizardWares.DataAccess.Repositiory.IRepository
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
 
--- WizardWares/Areas/Admin/Controllers/CategoryController.cs
+++ WizardWares/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WizardWares.DataAccess.Repositiory;
+using WizardWares.DataAccess.Repositiory.IRepository;
+using WizardWares.Models;
 
 namespace WizardWares.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            // Use entitiy framework core to retrieve the list
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
         }
     }
 }
