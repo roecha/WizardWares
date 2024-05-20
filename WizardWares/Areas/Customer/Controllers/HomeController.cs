8ed2b828f0254ad56a297f3c3f23b103880d98f7
roecha <croehl23@gmail.com>
2024-05-20 06:35:22 -0500

    Working on homepage. needs makeover

--- WizardWares/Areas/Customer/Controllers/HomeController.cs
+++ WizardWares/Areas/Customer/Controllers/HomeController.cs
@@ -1,23 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using WizardWares.DataAccess.Repositiory;
+using WizardWares.DataAccess.Repositiory.IRepository;
 using WizardWares.Models;
 
-namespace WizardWares.Areas.Customer.Controllers
+namespace TomesNScrolls.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
